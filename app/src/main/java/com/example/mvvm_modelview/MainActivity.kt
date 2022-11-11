9a3b06dbe2a616ac977a17082d68a4147e257cce
KashbullinRail <kashbullinrail@gmail.com>
2022-11-11 09:15:05 +0300

    реализована LiveData через ViewModel

--- app/src/main/java/com/example/mvvm_modelview/MainActivity.kt
+++ app/src/main/java/com/example/mvvm_modelview/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.mvvm_modelview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.inputmethod.InputBinding
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvm_modelview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var mviewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mviewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
+
+    override fun onStart() {
+        super.onStart()
+        mviewModel.liveData.observe(this, Observer {
+            binding.tvOne.text = it
+        })
+    }
+
+
+
 }
